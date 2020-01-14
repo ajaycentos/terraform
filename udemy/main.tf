ba7b0516ae6fb67b29437202e92d21f0d9c37b5d
Ajay Anand <ajaycentos@gmail.com>
2020-01-15 01:32:59 +0530

    added NIC code

--- udemy/main.tf
+++ udemy/main.tf
@@ -8,6 +8,8 @@ variable "web_server_rg" {}
 variable "resource_prefix" {}
 variable "web_server_address_space" {}
 variable "web_server_address_prefix" {}
+variable "web_server_name" {}
+
 
 
 
@@ -40,5 +42,19 @@ resource "azurerm_subnet" "web_server_subnet" {
   resource_group_name=azurerm_resource_group.web_server_rg.name
   virtual_network_name=azurerm_virtual_network.web_server_vnet.name
   address_prefix=var.web_server_address_prefix  
+  
 }
+
+resource "azurerm_network_interface" "web_server_nic" {
+  name="${var.web_server_name}-nic"
+       location=var.web_server_location
+     resource_group_name=azurerm_resource_group.web_server_rg.name
+     ip_configuration {
+       name="${var.web_server_name}-nic"
+       subnet_id=azurerm_subnet.web_server_subnet.id 
+       private_ip_address_allocation="dynamic"
+     }
+
+}
+
 
